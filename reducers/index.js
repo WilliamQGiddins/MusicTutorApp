a61926f17358584885cb41b3f80c2ddb66fdbc04
WilliamQGiddins <51344197+WilliamQGiddins@users.noreply.github.com>
2019-07-15 15:06:48 -0400

    Delete index.js

--- reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import midi, * as fromMidi from './midi'
-
-export default combineReducers({
-  midi
-})
-
-export const getMidi = state => 
-  fromMidi.getMidi(state)
